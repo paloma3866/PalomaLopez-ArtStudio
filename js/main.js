a7d2843ef623d505696c1f9dddc175c2203218fb
paloma3866 <lopezpaloma483@gmail.com>
2025-08-16 13:25:13 -0300

    Subiendo actualización con algunos cambios

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+//js básico para el acordeón
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+  header.addEventListener('click', () => {
+    const item = header.parentElement;
+    item.classList.toggle('active');
+  });
+});
