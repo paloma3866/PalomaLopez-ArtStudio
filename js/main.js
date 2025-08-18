5bbf93dcb0f75d33f4a3e1455d923e69cf798dfa
paloma3866 <lopezpaloma483@gmail.com>
2025-08-17 22:57:56 -0300

    botón hamburguesa agregado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 
-//js básico para el acordeón
+
+// ACORDEÓN
+
 const headers = document.querySelectorAll('.accordion-header');
 
 headers.forEach(header => {
   header.addEventListener('click', () => {
     const item = header.parentElement;
     item.classList.toggle('active');
+  });
+});
+
+
+// MENÚ HAMBURGUESA
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.navegar ol');       
+const navLinks = document.querySelectorAll('.navegar ol li a'); 
+
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('active'); 
+  menuToggle.classList.toggle('open'); 
+});
+
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (navList.classList.contains('active')) {
+      navList.classList.remove('active');
+      menuToggle.classList.remove('open');
+    }
   });
 });
